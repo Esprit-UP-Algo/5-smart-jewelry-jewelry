5ff8a2cba989b37c7be3c65d027ecaa2ccead8b0
Samar <souissi.samar@esprit.tn>
2023-10-24 23:35:46 +0200

    First commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+
+
+
+int main(int argc, char *argv[])
+{
+cout<<"Gestion Vlients"<<endl;
+QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+
+
+
+
+ return a.exec();
+
+}
